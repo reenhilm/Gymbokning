41cc8d1af2fdf9dc853e644362820517a522e750
reenhilm <christian@kajal.se>
2022-08-19 09:22:51 +0200

    re-scaffolded, added using statement

--- Gymbokning/Data/ApplicationDbContext.cs
+++ Gymbokning/Data/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
 ﻿using Gymbokning.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Gymbokning.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole,
+string>
+
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
---
+++ Gymbokning/Data/Migrations/20220819071735_AddedIdentityDbContextTypes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gymbokning.Data.Migrations
+{
+    public partial class AddedIdentityDbContextTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+
+            migrationBuilder.UpdateData(
+                table: "GymClass",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "StartTime",
+                value: new DateTime(2022, 8, 19, 10, 17, 35, 278, DateTimeKind.Local).AddTicks(8825));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "GymClass",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "StartTime",
+                value: new DateTime(2022, 8, 19, 9, 59, 20, 734, DateTimeKind.Local).AddTicks(6412));
+        }
+    }
+}
