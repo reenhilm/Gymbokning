a0c70c74dbf80f2d4f0eabd25fbcc1d79a613011
reenhilm <christian@kajal.se>
2022-08-26 12:33:37 +0200

    added PersonalData dataannotations

--- Gymbokning/Models/ApplicationUser.cs
+++ Gymbokning/Models/ApplicationUser.cs
@@ -5,10 +5,12 @@ namespace Gymbokning.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [PersonalData]
         [Required]
         [StringLength(50, MinimumLength = 3)]
         [Display(Name = "First Name")]
         public string FirstName { get; set; } = default!;
+        [PersonalData]
         [Required]
         [StringLength(50, MinimumLength = 3)]
         [Display(Name = "Last Name")]
